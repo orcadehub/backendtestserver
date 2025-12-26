85cae24bd117505767fb3cb568cbc38e7acba13e
orcadehub <orcadehub2@gmail.com>
2025-12-26 19:33:43 +0530

    Update 1.0.1

--- server.js
+++ server.js
@@ -9,21 +9,4 @@ app.listen(3000, () => {
 });
 
 
-// Github Account creation
-// Download and Install Git from https://git-scm.com/downloads
-// Open terminal and run the following commands
 
-// git config --global user.name "Your Name"
-// git config --global user.email "<your_email@example.com>"
-
-// git config --list               -> to verify
-
-
-
-// git init
-// git remote add origin <repository_url>
-// git branch -M main
-
-// git add .
-// git commit -m "Initial commit"
-// git push -u origin main
