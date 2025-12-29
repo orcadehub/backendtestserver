8d1828c9113abfc50bb77be1c34c80eecc067f04
orcadehub <orcadehub2@gmail.com>
2025-12-29 19:55:31 +0530

    update 1.0.3

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 import express from "express";
-import TestRoutes from './routes/test_route.js'
+import TestRoutes from "./routes/test_route.js";
 const app = express();
 app.use(express.json());
-app.use(TestRoutes)
+app.use(TestRoutes);
 
 app.get("/", (req, res) => {
   res.send("My Api is Working!");
 });
 
+users = [
+  { id: 1, name: "Rahul" },
+  { id: 2, name: "Rohit" },
+  { id: 3, name: "Raj" },
+];
+
+app.get("/user/:id", (req, res) => {
+  const userId = parseInt(req.params.id);
+  const user = users.find((u) => u.id === userId);
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
-
-
