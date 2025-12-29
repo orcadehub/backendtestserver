25e6d2cd02bad7971100b05a2481b33b9ff8534a
orcadehub <orcadehub2@gmail.com>
2025-12-29 19:47:02 +0530

    update 1.0.2

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const app = express();
 app.use(express.json());
 app.use(TestRoutes)
 
+app.get("/", (req, res) => {
+  res.send("My Api is Working!");
+});
+
 app.listen(3000, () => {
   console.log("Server is running on http://localhost:3000");
 });
-
 
 
